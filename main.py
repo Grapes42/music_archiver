eaad3fe7085ea68292c47ce37bc858afc36ee459
Grapes42 <f.inggrapes@gmail.com>
2025-04-08 21:02:50 +1200

    separated work into multiple files

--- main.py
+++ main.py
@@ -1,71 +1,77 @@
 import os
+import eyed3
 
-# Class Setup
-class MusicFile():
-    def __init__(self, title, link, album=""):
-        self.title = title
-        self.album = album
-        self.link = link
-
-        if self.album == "":
-            self.album = self.title
-
-class MultiAlbum():
-    def __init__(self, title, music_files):
-        self.title = title
-        self.music_files = music_files
-
-class Artist():
-    def __init__(self, title, music_files=[], multi_albums=[]):
-        self.title = title
-        self.music_files = music_files
-        self.multi_albums = multi_albums
-
-    def __str__(self):
-        string = ""
-        for file in self.music_files:
-            string += f"Title: {file.title}\n"
-            string += f"Album: {file.album}\n"
-            string += f"Link: {file.link}\n"
-        return string
-
-
+from map import return_artists
 
 # yt dlp setup
 def download_from_youtube(title, link):
     cmd = f"yt-dlp --cookies-from-browser firefox -x --audio-quality 0 --audio-format mp3 \"{link}\" -o \"{title}\""
     os.system(cmd)
 
+def add_metadata(path, title, artist, album, track=1):
+    d3_file = eyed3.load(path)
+
+    d3_file.tag.title = title
+    d3_file.tag.artist = artist
+    d3_file.tag.album = album
+    d3_file.tag.track_num = track
+
+    d3_file.tag.save()
+
 
 # Main loop
-def main():
-    artists = return_artists()
+artists = return_artists()
 
-    artist_count = len(artists)
+artist_count = len(artists)
 
-    if input("Replace mode? (y/n): ").lower() == "y":
-        replace_mode = True
-    else:
-        replace_mode = False
+if input("Replace mode? (y/n): ").lower() == "y":
+    replace_mode = True
+else:
+    replace_mode = False
 
-    # Main loop
-    for i in range(artist_count):
+# Main loop
+for i in range(artist_count):
 
-        artist = artists[i]
+    artist = artists[i]
 
-        print(f"\n\nDownloading {artist.title}, artist {i+1} of {artist_count}")
-        print("-"*5)
+    print(f"\n\nDownloading {artist.title}, artist {i+1} of {artist_count}")
+    print("-"*5)
 
-        # Create folder for artist
-        if not os.path.exists(artist.title):
-            os.makedirs(artist.title)
+    # Create folder for artist
+    if not os.path.exists(artist.title):
+        os.makedirs(artist.title)
 
-        # Download all single files
-        for file in artist.music_files:
-            file_title = f"{artist.title}/{file.title}"
+    # Download all single files
+    for file in artist.music_files:
+        file_title = f"{artist.title}/{file.title}"
 
-            # Skip download if not in replace 
-            # mode and file already exists
+        # Skip download if not in replace 
+        # mode and file already exists
+        if not replace_mode:
+            if os.path.isfile(f"{file_title}.mp3"):
+                print("File already exists, skipping.")
+                continue
+
+        download_from_youtube(title=file_title, link=file.link)
+
+        add_metadata(path=f"{file_title}.mp3", 
+                        title=file.title,
+                        artist=artist.title,
+                        album=file.title.replace(" - Album", ""))
+
+
+
+
+    # Download all multi albums
+    for multi_album in artist.multi_albums:
+        if not os.path.exists(f"{artist.title}/{multi_album.title}"):
+            os.makedirs(f"{artist.title}/{multi_album.title}")
+
+        track = 1
+
+        for file in multi_album.music_files:
+            file_title = f"{artist.title}/{multi_album.title}/{track} - {file.title}"
+
             if not replace_mode:
                 if os.path.isfile(f"{file_title}.mp3"):
                     print("File already exists, skipping.")
@@ -73,78 +79,9 @@ def main():
 
             download_from_youtube(title=file_title, link=file.link)
 
+            add_metadata(path=f"{file_title}.mp3",
+                            title=file.title,
+                            artist=artist.title,
+                            album=multi_album.title.replace(" - Album", ""))
             
-
-
-        # Download all multi albums
-        for multi_album in artist.multi_albums:
-            if not os.path.exists(f"{artist.title}/{multi_album.title}"):
-                os.makedirs(f"{artist.title}/{multi_album.title}")
-
-            for file in multi_album.music_files:
-                file_title = f"{artist.title}/{multi_album.title}/{file.title}"
-
-                if not replace_mode:
-                    if os.path.isfile(f"{file_title}.mp3"):
-                        print("File already exists, skipping.")
-                        continue
-
-                download_from_youtube(title=file_title, link=file.link)
-
-
-
-# Music list
-def return_artists():
-    artists = []
-
-    artists.append(
-        Artist(title="Butcher M.D.",
-            music_files=[
-                MusicFile(title="Traces of Gore - Album", link="https://www.youtube.com/watch?v=fSCv6MswW8E")
-            ])
-    )
-
-    artists.append(
-        Artist(title="Esophagus",
-            music_files=[
-                MusicFile(title="Killing for Sport - EP", link="https://www.youtube.com/watch?v=X_U4WZ-9hf0"),
-                MusicFile(title="Steamrollin'", link="https://www.youtube.com/watch?v=-zZUh-Pcs-E")
-            ])
-    )
-
-    artists.append(
-        Artist(title="Gore Instinct",
-            music_files=[
-                MusicFile(title="Invasion Of The Body Slammer - Album", link="https://www.youtube.com/watch?v=8EUgH9y_D0I")
-            ])
-    )
-
-    artists.append(
-        Artist(title="The Dark Prison Massacre",
-            multi_albums=[
-                MultiAlbum(title="Deformity of Human Consciousness - Album",
-                        music_files=[
-                            MusicFile(title="1 - Silence of Decay", link="https://www.youtube.com/watch?v=bsBJwnXMRRI"),
-                            MusicFile(title="2 - Illusion of Withering", link="https://www.youtube.com/watch?v=ad0U37c4GJw"), 
-                            MusicFile(title="3 - Hate Purgatory", link="https://www.youtube.com/watch?v=FPZSv8__mjw"),
-                            MusicFile(title="4 - Despite of Desolation", link="https://www.youtube.com/watch?v=VqY1u5_hIjk"),
-                            MusicFile(title="5 - Mortal Awareness", link="https://www.youtube.com/watch?v=ztU6fc8cbTo"),
-                            MusicFile(title="6 - Narcotic Induced Hypo-Thermia", link="https://www.youtube.com/watch?v=B1fgKd8lUwQ"),
-                            MusicFile(title="7 - Faget (Korn Cover)", link="https://www.youtube.com/watch?v=gQfs1jRbzjs"),
-                            MusicFile(title="8 - Crematory", link="https://www.youtube.com/watch?v=WOxzVUXlY4M"),
-                            MusicFile(title="9 - Electric Shock", link="https://www.youtube.com/watch?v=7aCHhbGMLp8"),
-                        ]
-                    )
-            ])
-    )
-
-    artists.append(
-        Artist(title="Visceral Disgorge",
-               music_files=[
-                   MusicFile(title="Ingesting Putridity - Album", link="https://www.youtube.com/watch?v=Qqa0v79VutY")
-               ])
-    )
-
-    return artists
-
-main()
+            track += 1
---
+++ map.py
@@ -0,0 +1,115 @@
+from classes import *
+
+# Music list
+def return_artists():
+    artists = []
+
+    artists.append(
+        Artist(title="Butcher M.D.",
+            music_files=[
+                MusicFile(title="Traces of Gore - Album", link="https://www.youtube.com/watch?v=fSCv6MswW8E")
+            ])
+    )
+
+    artists.append(
+        Artist(title="Esophagus",
+            music_files=[
+                MusicFile(title="Killing for Sport - EP", link="https://www.youtube.com/watch?v=X_U4WZ-9hf0"),
+                MusicFile(title="Steamrollin'", link="https://www.youtube.com/watch?v=-zZUh-Pcs-E")
+            ])
+    )
+
+    artists.append(
+        Artist(title="Gore Instinct",
+            music_files=[
+                MusicFile(title="Invasion Of The Body Slammer - Album", link="https://www.youtube.com/watch?v=8EUgH9y_D0I")
+            ])
+    )
+
+    artists.append(
+        Artist(title="Sanguisugabogg",
+            music_files=[
+                MusicFile(title="Tortured Whole", link="https://www.youtube.com/watch?v=yva43MGiM0w")
+            ],
+            multi_albums=[
+                MultiAlbum(title="Homicidal Ecstacy - Album", 
+                    music_files=[
+                        MusicFile(title="Black Market Vasectomy", link="https://www.youtube.com/watch?v=StMpkOd-Na8"),
+                        MusicFile(title="Face Ripped Off", link="https://www.youtube.com/watch?v=rEtoUc9n1vo"),
+                        MusicFile(title="Pissed", link="https://www.youtube.com/watch?v=wReUTVtsYks"),
+                        MusicFile(title="Testicular Rot", link="https://www.youtube.com/watch?v=XxQ_iM8Jl1g"),
+                        MusicFile(title="Hungry for Your Insides", link="https://www.youtube.com/watch?v=HsSNZ8Sm5Fo"),
+                        MusicFile(title="Skin Cushion", link="https://www.youtube.com/watch?v=A97r6Ufzr6A"),
+                        MusicFile(title="A Lesson in Savagery", link="https://www.youtube.com/watch?v=DpQgyUe7XXQ"),
+                        MusicFile(title="Narcissistic Incisions", link="https://www.youtube.com/watch?v=iE4BFIq9b_E"),
+                        MusicFile(title="Mortal Admonishment", link="https://www.youtube.com/watch?v=k5-coaM3DQI"),
+                        MusicFile(title="Proclamation of the Frail", link="https://www.youtube.com/watch?v=iB9t_tNITnY"),
+                        MusicFile(title="Necrosexual Deviant", link="https://www.youtube.com/watch?v=vn2PkwULBVk"),
+                        MusicFile(title="Feening for Bloodshed", link="https://www.youtube.com/watch?v=NtZPkzep_r8")
+                    ])
+            ])
+            
+
+    )
+
+    artists.append(
+        Artist(title="Satan's Revenge on Mankind",
+            multi_albums=[
+                MultiAlbum(title="Goreblast",
+                    music_files=[
+                        MusicFile(title="Vaticandestruction", link="https://www.youtube.com/watch?v=vQmMFuAp54k"),
+                        MusicFile(title="We Come To Eat You", link="https://www.youtube.com/watch?v=4bgctFIchjU"),
+                        MusicFile(title="Gorevolution", link="https://www.youtube.com/watch?v=edxovxnKJqI"),
+                        MusicFile(title="Hyperbrutal Splattersatan", link="https://www.youtube.com/watch?v=qtMHIjaasfo"),
+                        MusicFile(title="I Hate Music", link="https://www.youtube.com/watch?v=vyzBoAJV-8s"),
+                        MusicFile(title="Invoking the Glorious Sado Nuclear Extermination of the Christian Plague", link="https://www.youtube.com/watch?v=lTL90ZGNDZk"),
+                        MusicFile(title="Esto Es Real", link="https://www.youtube.com/watch?v=LL1ywKRGqzk"),
+                        MusicFile(title="Psychopunch", link="https://www.youtube.com/watch?v=feN1PUQ98Yc"),
+                        MusicFile(title="Satan Rules", link="https://www.youtube.com/watch?v=KMiYLa7EBik"),
+                        MusicFile(title="C.M.F.P", link="https://www.youtube.com/watch?v=dG3Yu5Np3VM"),
+                        MusicFile(title="I Hate Nature", link="https://www.youtube.com/watch?v=G4zasX6SdTk"),
+                        MusicFile(title="Meat Is Good Food", link="https://www.youtube.com/watch?v=m4DyXNb56XU"),
+                        MusicFile(title="Goat Gut Gore Core", link="https://www.youtube.com/watch?v=Pz7HcRk6g8Y"),
+                        MusicFile(title="Jesus Loves You Because He Is Gay", link="https://www.youtube.com/watch?v=yYvbr374EkI"),
+                        MusicFile(title="Angels Taste Like Chicken", link="https://www.youtube.com/watch?v=jgzzy0MTB1Q"),
+                        MusicFile(title="Body Juice Blues", link="https://www.youtube.com/watch?v=P3fgahRXQ-E"),
+                        MusicFile(title="I Hate Neo Grunge Pseudo Punk Hippie Brats", link="https://www.youtube.com/watch?v=1BDQGdxyhbU"),
+                        MusicFile(title="Belial's Finger Sticks in the Asshole of the Pope", link="https://www.youtube.com/watch?v=v--CHmh2OfI"),
+                        MusicFile(title="Fuck You!", link="https://www.youtube.com/watch?v=E3R2NPtYmPQ"),
+                        MusicFile(title="I Vomit on the Altar and Shit in the Offertory Bag", link="https://www.youtube.com/watch?v=ucc9xYlVsOs"),
+                        MusicFile(title="Techno Junkie Massacre", link="https://www.youtube.com/watch?v=MtsDzuKmwSw"),
+                        MusicFile(title="Eat! Eat! Eat!", link="https://www.youtube.com/watch?v=hCdsfvZRGF0"),
+                        MusicFile(title="World War Gore", link="https://www.youtube.com/watch?v=vpm57X7UYLE"),
+                        MusicFile(title="Infernal Christ Corpse Hacking Butcher Command on Behalf of the Great Eternal Master of Hell's Purifying Fire", link="https://www.youtube.com/watch?v=yl5YVN2wLwQ"),
+                    ]               
+            )
+        ])
+    )
+
+    artists.append(
+        Artist(title="The Dark Prison Massacre",
+            multi_albums=[
+                MultiAlbum(title="Deformity of Human Consciousness - Album",
+                    music_files=[
+                        MusicFile(title="Silence of Decay", link="https://www.youtube.com/watch?v=bsBJwnXMRRI"),
+                        MusicFile(title="Illusion of Withering", link="https://www.youtube.com/watch?v=ad0U37c4GJw"), 
+                        MusicFile(title="Hate Purgatory", link="https://www.youtube.com/watch?v=FPZSv8__mjw"),
+                        MusicFile(title="Despite of Desolation", link="https://www.youtube.com/watch?v=VqY1u5_hIjk"),
+                        MusicFile(title="Mortal Awareness", link="https://www.youtube.com/watch?v=ztU6fc8cbTo"),
+                        MusicFile(title="Narcotic Induced Hypo-Thermia", link="https://www.youtube.com/watch?v=B1fgKd8lUwQ"),
+                        MusicFile(title="Faget (Korn Cover)", link="https://www.youtube.com/watch?v=gQfs1jRbzjs"),
+                        MusicFile(title="Crematory", link="https://www.youtube.com/watch?v=WOxzVUXlY4M"),
+                        MusicFile(title="Electric Shock", link="https://www.youtube.com/watch?v=7aCHhbGMLp8")
+                    ]
+                )
+            ])
+    )
+
+    artists.append(
+        Artist(title="Visceral Disgorge",
+               music_files=[
+                   MusicFile(title="Ingesting Putridity - Album", link="https://www.youtube.com/watch?v=Qqa0v79VutY")
+               ])
+    )
+
+    return artists
